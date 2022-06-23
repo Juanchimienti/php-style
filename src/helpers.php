c4d0c7f2586fb606d0d72ddb25e44cf99bc08c13
Juan Chimienti <juan.chimienti@gmail.com>
2022-06-23 10:23:57 -0300

    Added more rules

--- src/helpers.php
+++ src/helpers.php
@@ -9,14 +9,29 @@ function styles(\PhpCsFixer\Finder $finder, array $rules = []): \PhpCsFixer\Conf
 		'indentation_type' => TRUE,
 		'method_chaining_indentation' => TRUE,
 		'array_indentation' => TRUE,
-		'array_syntax' => ['syntax' => 'short'],
+		'array_syntax' => [
+			'syntax' => 'short',
+		],
 		'braces' => [
 			'position_after_anonymous_constructs' => 'next', // this is for functions
-			'position_after_control_structures' => 'next' // this for if-else structures
+			'position_after_control_structures' => 'next', // this for if-else structures
 		],
 		'constant_case' => [
-			'case' => 'upper'
-		]]);
+			'case' => 'upper',
+		],
+		'no_multiline_whitespace_around_double_arrow' => TRUE,
+		'binary_operator_spaces' => [
+			"operators" => [
+				"=>" => "single_space",
+			],
+		],
+		'trailing_comma_in_multiline' => TRUE,
+		'blank_line_before_statement' => [
+			'statements' => ['return'],
+		],
+		'no_unused_imports' => TRUE,
+		'no_blank_lines_after_class_opening' => TRUE,
+	]);
 	$config = new \PhpCsFixer\Config();
 
 	return $config->setRules($rules)
